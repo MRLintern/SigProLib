926334fea1977257d6536356c4db1aee7c7342f9
ProgrammagableScience <86484124+MRLintern@users.noreply.github.com>
2025-10-01 15:24:53 +0100

    initial commit

---
+++ Fourier_Transform/FFT/include/constants.hpp
@@ -0,0 +1,11 @@
+// constants.hpp
+
+#pragma once
+
+#include <cstdint>
+
+// number of samples in the input signal
+inline constexpr std::uint32_t SIG_LENGTH = 320u;
+
+// PI constant
+inline constexpr double PI = 3.141592653589793238462643383279502884;
